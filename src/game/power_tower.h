d350dabb002823dd69c3abae4629d83959b6c9fa
JaviCervera <JaviCervera@users.noreply.github.com>
2026-09-06 17:37:50 +0200

    Change power_tower_t action message

--- src/game/power_tower.h
+++ src/game/power_tower.h
@@ -8,7 +8,7 @@ struct power_tower_t : public actionable_t
 	power_tower_t(vec2_t pos) : actionable_t{POWER_TOWER_NAME, pos, POWER_TOWER_ID}
 	{
 		collidable = true;
-		action_text = "Connect power generator";
+		action_text = "Place power generator";
 	}
 
 	void on_action_pressed() override
